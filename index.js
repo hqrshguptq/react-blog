6ea1cbea7c15ab6addf8a584787e57151215f7bb
Harsh Gupta <harshgupta@Harshs-MacBook-Air.local>
2020-03-20 23:16:36 +0530

    hash

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const config =require('./config/key')
+
+const config =require('./config/key');
 
 const { User} = require("./models/user");
+const {auth} =require("./middleware/auth");
 
 mongoose.connect(config.mongoURI,
 {useNewUrlParser:true}).then(()=>console.log('DB connected'))
@@ -17,9 +19,16 @@ mongoose.connect(config.mongoURI,
  app.use(bodyParser.json());
  app.use(cookieParser());      
  
- app.get('/',(req,res)=>{
-     res.json({"hello~":"adsssssf"})
- })
+ app.get('/api/user/auth', auth ,(req,res)=>{
+    res.status(200).json({
+     _id:req._id,
+     isAuth:true,
+     email:req.user.email,
+     name:req.user.name,
+     lastname:req.user.lastname,
+     role:req.user.role
+    })
+})
 
  app.post('/api/users/register',(req,res)=>{
     const user= new User(req.body)
@@ -31,4 +40,34 @@ mongoose.connect(config.mongoURI,
             })
         })
  })
+
+app.post('/api/user/login',(req,res)=>{
+//email
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(!user)
+        return res.json({
+            loginSuccess: false,
+            message: " Auth failed ! email not found"
+    });
+})
+//comparePass
+user.comparePassword(req.body.password,(err,isMatch)=>{
+    if (!isMatch){
+        return res.json({loginSuccess:false,message:"wrong password"})
+    }
+})
+//generateToken
+user.generateToken((err,user)=>{
+    if (err) return res.status(400).send(err);
+    res.cookie("x_auth",user.token)
+    .status(200)
+    .json({
+        loginSuccess:true
+    })
+})
+
+
+})
+
+
 app.listen(5000);
